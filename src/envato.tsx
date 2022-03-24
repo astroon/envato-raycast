2ee47ad8f0786cc0745f1e64014d06ec9a0c74a8
Slava Kharkov <Slavikharkov01@gmail.com>
2022-03-24 22:01:10 +0200

    update

--- src/envato.tsx
+++ src/envato.tsx
@@ -6,6 +6,8 @@ import {
   showToast,
   ToastStyle,
   Detail,
+  Icon,
+  Color,
 } from "@raycast/api";
 import { useState, useEffect } from "react";
 import Envato from "envato";
@@ -24,18 +26,29 @@ type envatoErrors = {
   description?: string;
 };
 
+type envatoUser = {
+  username?: string;
+  sales?: string;
+  image?: string;
+};
+
 export default function Command() {
-  const [state, setState] = useState<{ sales: []; errors: envatoErrors }>({ sales: [], errors: [] as envatoErrors });
+  const [state, setState] = useState<{ account: []; user: envatoUser; sales: []; errors: envatoErrors }>({ account: [], user: [] as envatoUser, sales: [], errors: [] as envatoErrors });
 
   useEffect(() => {
 	async function fetch() {
 	  try {
 		const client = Envato !== undefined ? new Envato.Client(token) : undefined;
+		const username = client !== undefined ? await client.private.getUsername() : "";
+		const userInfo = client !== undefined ? await client.user.getAccountDetails(username) : [];
+		const accountInfo = client !== undefined ? await client.private.getAccountDetails() : [];
 		const salesInfo = client !== undefined ? await client.private.getSales() : [];
 		const salesEmpty: any = salesInfo.length === 0 ? { empty: true } : [];
 		setState((oldState) => ({
 		  ...oldState,
 		  sales: salesInfo as [],
+		  user: userInfo as envatoUser,
+		  account: accountInfo as [],
 		  errors: salesEmpty as envatoErrors,
 		}));
 	  } catch (error: any) {
@@ -77,8 +90,9 @@ export default function Command() {
 		icon={props.sale.item.previews.icon_preview.icon_url ?? "/"}
 		title={String(props.sale.item.name ?? "")}
 		subtitle={String(dateFormat(props.sale.sold_at, "dd.mm.yyyy")) ?? ""}
-		accessoryIcon="💵"
-		accessoryTitle={`${price(props.sale.amount, props.sale.support_amount)}`}
+		accessories={[
+		  { text: `${price(props.sale.amount, props.sale.support_amount)}`, icon: Icon.List },
+		]}
 		actions={
 		  <ActionPanel>
 			<OpenInBrowserAction url={`${props.sale.item.url}`} />
@@ -87,9 +101,32 @@ export default function Command() {
 	  />
 	);
   }
+  function Account(props: { infoUser: any; infoAccount: any;}) {
+	return (
+	<List.Item
+	  icon={props.infoUser.image ?? "/"}
+	  title={String(`${props.infoUser.username}` ?? "")}
+	  subtitle={String(`${props.infoAccount.firstname} ${props.infoAccount.surname}`) ?? ""}
+	  accessories={[
+		{ text: `${props.infoUser.sales}`, icon: { source: Icon.Calendar, tintColor: Color.Green } },
+		{ text: `$${props.infoAccount.balance}`, icon: { source: Icon.List, tintColor: Color.Green } },
+	  ]}
+	/>
+  );
+  }
 
   return (
 	<List isLoading={state.sales.length === 0 && state.errors.reason == undefined && state.errors.empty !== true}>
+	  <List.Section title="Account">
+		  {state.user.username === "" ||  state.user.username == undefined ? (
+			  <List.EmptyView
+				icon={{ source: Icon.Person }}
+				title="Loading..."
+			  />
+			) : (
+			  <Account infoUser={state.user} infoAccount={state.account}/>
+		   )}
+	  </List.Section>
 	  <List.Section title="Today">
 		{state.sales.map((sale, index) => {
 		  const saleDate = String(dateFormat(sale["sold_at"], "dd, mm, yyyy"));
@@ -97,10 +134,17 @@ export default function Command() {
 		})}
 	  </List.Section>
 	  <List.Section title="Sales">
-		{state.sales.map((sale, index) => {
-		  const saleDate = String(dateFormat(sale["sold_at"], "dd, mm, yyyy"));
-		  if (saleDate != fullDate && state.errors !== []) return <SaleItem sale={sale} key={index} />;
-		})}
+		  {state.user.username === "" ||  state.user.username == undefined ? (
+				<List.EmptyView
+				  icon={{ source: Icon.TwoArrowsClockwise }}
+				  title="Loading..."
+				/>
+			  ) : (
+				state.sales.map((sale, index) => {
+				  const saleDate = String(dateFormat(sale["sold_at"], "dd, mm, yyyy"));
+				  if (saleDate != fullDate && state.errors !== []) return <SaleItem sale={sale} key={index} />;
+				})
+			 )}
 	  </List.Section>
 	</List>
   );
